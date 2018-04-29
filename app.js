c71136a3780b842ea010515af7a0c8b51fbaaec5
Marco Caldas <marcoapcaldas@gmail.com>
2018-04-29 01:48:09 -0300

    Added the code to check if the clicked color is the right and added some space between the squares

--- app.js
+++ app.js
@@ -26,7 +26,20 @@ function newColors(){
 			squares[i].bgColor =  "rgb("+tempRGB[0]+", "+tempRGB[1]+", "+tempRGB[2]+")";
 		} 
 	}
-		
-	};
+};
+
+for (let i = 0; i < squares.length; i++){
+	squares[i].addEventListener("click", function(){
+		choose = this.bgColor.match(/\d+/g);
+		choose[0] = parseInt(choose[0]);
+		choose[1] = parseInt(choose[1]);
+		choose[2] = parseInt(choose[2]);
+		console.log(choose);
+		console.log(tempRGB);
+		if (choose.toString() == tempRGB.toString()){
+			console.log("Ganhou");
+		}
+	});
+};
 
 newColors();
